3ef5b6c5ee1d14e194b6ab7393a64d68a3e338ee
Kendra Tang <st870522@gmail.com>
2015-05-20 15:20:25 +0800

    finish send confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,17 @@
 Rails.application.configure do
+  # http://stackoverflow.com/questions/8186584/how-do-i-set-up-email-confirmation-with-devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  # https://ihower.tw/rails4/actionmailer.html
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.mandrillapp.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :authentication => "plain",
+    :user_name => "st870522@gmail.com",
+    :password => "g-ttLrdTZHe62jvPnE-cCA",
+    :enable_starttls_auto => true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -14,7 +27,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
