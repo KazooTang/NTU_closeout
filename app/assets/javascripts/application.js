a5f49dfe41d8946ae49e6bbe326be5fcecf7a715
Kendra Tang <st870522@gmail.com>
2015-06-14 23:21:52 +0800

    divide productlist -> sqr -> modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,3 @@
 //= require jquery
 //= require bootstrap-sprockets
 
-$('#modal').on('show.bs.modal', function () {
-  $(this).find('.modal-body').css({
-    width:'auto', //probably not needed
-    height:'auto', //probably not needed 
-    'max-height':'100%'
-  });
-});
