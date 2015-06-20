3fa3c77622be6a7bd607da96c07bf89e6bc1c60f
Kendra Tang <st870522@gmail.com>
2015-06-20 17:59:28 +0800

    fix search problem

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,17 +11,19 @@ class Product < ActiveRecord::Base
   end
 
   def self.selling
-    where.not('message like ? OR message like ? OR message like ?', '%SOLD%', '%已售出%', '%(DELETED)%').where.not(message: nil)
+    where.not([(['(p_name like ? OR message like ?)'] * 2).join(' OR ')] + ['%SOLD%', '%DELETED%'].map{|x| [x] * 2}.flatten).where.not(message: nil)
+#    where.not('p_name like ? OR p_name like ? OR p_name like ?', '%SOLD%', '%已售出%', '%(DELETED)%').where.not(message: nil)
   end
 
   def self.sold
-    where('message like ? OR message like ? OR message like ?', '%SOLD%', '%已售出%', '%(DELETED)%')
+    where([(['(p_name like ? OR message like ?)'] * 2).join(' OR ')] + ['%SOLD%', '%DELETED%'].map{|x| [x] * 2}.flatten)
+#    where('p_name like ? OR p_name like ? OR p_name like ?', '%SOLD%', '%已售出%', '%(DELETED)%')
   end
 
   def self.search(search)
     if search
       search_length = search.split.length
-      where([(['(message like ? OR u_name like ?)'] * search_length).join(' AND ')] + search.split.map{|x| ["%#{x}%"] * 2}.flatten)
+      where([(['(p_name like ? OR u_name like ?)'] * search_length).join(' AND ')] + search.split.map{|x| ["%#{x}%"] * 2}.flatten)
     else
       all
     end
